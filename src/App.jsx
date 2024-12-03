e0bd644eb11b9eeeec0c69a3a0857d8a8621426d
newwmee <dmstjs365@gmail.com>
2024-12-03 17:12:24 +0900

    기본 설정 + 페이지 구성

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useEffect } from "react";
 import "./App.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMultiplePoketmonById } from "./RTK/thunk";
+import { Link, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import Detail from "./pages/Detail";
+import Favorite from "./pages/Favorite";
+import Search from "./pages/Search";
 
 function App() {
   const ditpatch = useDispatch();
@@ -13,7 +18,19 @@ function App() {
   }, []);
   return (
     <>
-      <h1 className="text-[40px]">포켓몬 도감</h1>
+      <h1 className="text-[40px] text-center">포켓몬 도감</h1>
+      <nav>
+        <Link to={"/"}>메인</Link>
+        <Link to={"/detail"}>상세정보</Link>
+        <Link to={"/search"}>검색</Link>
+        <Link to={"/favorite"}>찜목록</Link>
+      </nav>
+      <Routes>
+        <Route path={"/"} element={<Main />} />
+        <Route path={"/detail/:poketmon"} element={<Detail />} />
+        <Route path={"/search"} element={<Search />} />
+        <Route path={"/favorite"} element={<Favorite />} />
+      </Routes>
     </>
   );
 }
